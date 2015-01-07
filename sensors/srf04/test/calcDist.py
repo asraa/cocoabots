b1c77360a5f0f5b8c14df752a15615f7da09b761
cocoabot <cocoabot@mit.edu>
2015-01-07 19:27:50 +0000

    why won't thiswork?????

--- sensors/srf04/test/calcDist.py
+++ sensors/srf04/test/calcDist.py
@@ -5,18 +5,23 @@ import time
 
 trigpin = mraa.Gpio(13)
 echopin = mraa.Gpio(20)
-#echopin.enable(True)
-#echopin.enable(True)
+trigpin.dir(mraa.DIR_OUT)
+echopin.dir(mraa.DIR_IN)
+
+
+time.sleep(.3)
 
 for i in range(10):
     trigpin.write(0)
-    time.sleep(0.000002)
+    time.sleep(0.3)
     trigpin.write(1)
     time.sleep(.00001)
     trigpin.write(0)
+    #print echopin.read()
     #off = time.time()
     while echopin.read() == 0:
         off = time.time()
+    #print echopin.read()
     while echopin.read() == 1:
     #if echopin.read() == 1:
         on = time.time()
