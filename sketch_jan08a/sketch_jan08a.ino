b1ca30c33096b0a2f8e28b1133815d4de503d755
asra <asra@mit.edu>
2015-01-09 13:27:30 -0500

    more arduino tests

--- sketch_jan08a/sketch_jan08a.ino
+++ sketch_jan08a/sketch_jan08a.ino
@@ -2,8 +2,9 @@ const int echoPin = 3;
 const int trigPin = 2;
 
 void setup() {
-  pinMode(echoPin, OUTPUT);
-  pinMode(trigPin, INPUT); 
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT); 
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -15,6 +16,6 @@ void loop() {
   int distance = pulseIn(echoPin, HIGH);        // Read in times pulse
   distance= distance/58;                        // Calculate distance from time of pulse
   Serial.println(distance);                     
-  delay(50);                                    // Wait 50mS before next ranging
+  delay(100);                                    // Wait 50mS before next ranging
 }
 
