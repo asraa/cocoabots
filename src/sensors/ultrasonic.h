f88432cc088add4364ade5ec557e169dba9f0090
efjepsen <efjepsen@gmail.com>
2015-01-12 14:23:20 -0500

    dunno lol

--- src/sensors/ultrasonic.h
+++ src/sensors/ultrasonic.h
@@ -16,7 +16,7 @@ class ultrasonic
     void echo_handler(void* args);
 
   private:
-    mraa::Gpio TP_gpio, EP_gpio;
+    mraa::Gpio* TP_gpio, EP_gpio;
     int Trig_pin;
     int Echo_pin;
     // added this structure, I believe it's needed
