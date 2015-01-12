4b4388b557cdfa5af398824a5d3d4987cf0e7c08
asra <asra@mit.edu>
2015-01-12 15:14:36 -0500

    ok i lied i think shortir test is done?

--- src/sensors/shortIR_test.cpp
+++ src/sensors/shortIR_test.cpp
@@ -4,34 +4,8 @@
 #include <sys/time.h>
 #include "mraa.hpp"
 
-<<<<<<< HEAD
-int run_shortIR(){
-=======
-shortIR::shortIR(int dataPin){
-	mraa::Gpio* data_gpio = new mraa::Gpio(dataPin);
-	data_gpio->(mraa::DIR_IN);
-	if (data_gpio == NULL){
-		return MRAA_ERROR_UNSPECIFIED;
-	}
-	
-	mraa_result_t data_response = data_gpio->dir(mraa::DIR_IN);
-	if (data_response != MRAA_SUCCESS){
-		mraa::printError(data_response);
-		return 1;
-	}
-	data_pin = dataPin;
-}
-
-long shortIR::timing(){
-	return data_gpio->read();
-}
-
-long shortIR::ranging(int sys){
-	return 0;
-}
 
 int runShortIR(){
->>>>>>> 1d80377e48f15f93c0b5239beea6d8224432affb
 	usleep(50000.0);
 	shortIR aShortIR(2);
 	reading = aShortIR.timing();
