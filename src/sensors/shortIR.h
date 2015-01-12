7ea6c1eac66bae09e1841be684a012a2aaf04e84
asra <asra@mit.edu>
2015-01-12 14:21:34 -0500

    shortIR test code

--- src/sensors/shortIR.h
+++ src/sensors/shortIR.h
@@ -2,15 +2,19 @@
 #define SHORTIR_H
 
 
+#ifndef ULTRASONIC_CM 1
+#define ULTRASONIC_INC 0
+
 class shortIR
 {
   public:
     shortIR(int dataPin);
-    int getData();
+    long timing();
+    int ranging(int sys);
 
     private:
     int dataPin;
-
+    long duration, distance_cm, distance_inc;
     
 };
 
