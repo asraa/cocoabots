5573862c661f33c3eed3289c8b50d2945f46ace9
efjepsen <efjepsen@gmail.com>
2015-01-12 14:21:08 -0500

    updated ultrasonic.h

--- src/sensors/ultrasonic.h
+++ src/sensors/ultrasonic.h
@@ -5,6 +5,7 @@
 #define ULTRASONIC_INC 0
 
 #include <sys/time.h>
+#include "mraa.hpp"
 
 class ultrasonic
 {
@@ -15,6 +16,7 @@ class ultrasonic
     void echo_handler(void* args);
 
   private:
+    mraa::Gpio TP_gpio, EP_gpio;
     int Trig_pin;
     int Echo_pin;
     // added this structure, I believe it's needed
