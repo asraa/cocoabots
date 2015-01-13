2e9e4507e6ad001fd655e122ae1383377abbc9e5
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2015-01-13 13:48:16 -0500

    Extra test

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -36,8 +36,27 @@ int main(int argc, char** argv){
         }
     }
 
-    else if (strcmp(argv[1],"moveToWall")==0){
-      //moveToWallTest();
+    else if (strcmp(argv[1],"pwmservo")==0){
+        pwmUtils pwm;
+        //We have a servo in 0
+        while(1)
+        {
+            usleep(200000.0);
+            pwm.setServoPosition(0,0.3);
+            usleep(200000.0);
+            pwm.setServoPosition(0,0.5);
+            usleep(200000.0);
+            pwm.setServoPosition(0,1);
+            usleep(200000.0);
+            pwm.setServoPosition(0,2);
+            usleep(200000.0);
+            pwm.setServoPosition(0,3);
+            usleep(200000.0);
+            pwm.setServoPosition(0,20);
+
+
+
+        }
     }    
   }
  
