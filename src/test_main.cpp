b56268a4c5dcc4c219043f267687703f1d171aa5
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2015-01-13 20:59:55 -0500

    Adds test for sensor module

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include "sensors/ultrasonic.h"
 #include "actuators/pwmutils.h"
+#include "sensorsmodule.h"
 
 int main(int argc, char** argv){
   
@@ -42,19 +43,17 @@ int main(int argc, char** argv){
             pwm.setServoPosition(0,0.3);
             usleep(200000.0);
             pwm.setServoPosition(0,0.5);
-            usleep(200000.0);
-            pwm.setServoPosition(0,1);
-            usleep(200000.0);
-            pwm.setServoPosition(0,2);
-            usleep(200000.0);
-            pwm.setServoPosition(0,3);
-            usleep(200000.0);
-            pwm.setServoPosition(0,20);
-
-
-
         }
     }    
+    else if (strcmp(argv[1],"sensormoduleultrasonic")==0){
+        sensorsModule mysensors;
+        while(1)
+        {
+            printf("%f\n", mysensors.frontUltrasonicData);
+            usleep(200000.0);
+
+        }
+    }
   }
  
   return 0;
