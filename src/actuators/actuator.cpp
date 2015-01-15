64a9bf3dd022aac9bdd1e7305db85a4d8923425c
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2015-01-14 21:13:59 -0500

    Fixes bug set wheel speed

--- src/actuators/actuator.cpp
+++ src/actuators/actuator.cpp
@@ -14,6 +14,7 @@ void actuator::setPowerLeftWheel(double speed){
         dir =MOTOR_DIRECTION_LEFT;
     } else {
         dir =MOTOR_DIRECTION_RIGHT;
+        speed=-speed;
     }
     leftWheel.dirPin.write(dir);
     pwm.writePWM(leftWheel.pwmIndex,speed);
