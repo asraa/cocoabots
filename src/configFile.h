3f0055f5ff906ab3958b49bf656bae0249b7e109
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2015-01-15 17:35:12 -0500

    New parameters

--- src/configFile.h
+++ src/configFile.h
@@ -108,6 +108,6 @@
 #define SPEED_CONTROL_UPDATE_RATE_MILISECONDS 50
 #define FWD_ERROR_GAIN 0.0    //Should be positive
 #define FWD_SPEED_GAIN 0.0  //Should be negative
-#define ANG_ERROR_GAIN 0.005
-#define ANG_SPEED_GAIN 0.0  //Should be negative
+#define ANG_ERROR_GAIN 0.008
+#define ANG_SPEED_GAIN 0.002  //Should be negative
 #endif // CONFIGFILE_H
