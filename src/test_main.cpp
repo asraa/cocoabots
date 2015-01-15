bd2781935d7d7cccaaf22662317f0985e184ad83
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2015-01-14 20:24:27 -0500

    fixes test again

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -16,168 +16,169 @@ actuator * actPointer;
 int running;
 void stopMotors(int signo)
 {
-  if (signo == SIGINT) {
-    actPointer->setPowerLeftWheel(0);
-    actPointer->setPowerRightWheel(0);
-    running =0;
-  }
+    if (signo == SIGINT) {
+        actPointer->setPowerLeftWheel(0);
+        actPointer->setPowerRightWheel(0);
+        running =0;
+    }
 }
 
 int main(int argc, char** argv){
-  
-  //if no parameter was passed
-  if (argc==1){
-    printf("Missing Arguments\n The usage is: test_run arguments");
-  }
-  else{
-    if(strcmp(argv[1],"ultrasonic")==0){
-        printf("Running ultrasonic test\n");
-        ultrasonic testUltrasonic(2,3);
-        while(1)
-        {
-            usleep(200000.0);
-            printf("%f\n", testUltrasonic.getDistance(0));
-        }
 
+    //if no parameter was passed
+    if (argc==1){
+        printf("Missing Arguments\n The usage is: test_run arguments");
     }
-    else if (strcmp(argv[1],"ultrasonicpwm")==0){
-        ultrasonic testUltrasonic(2,3);
-        pwmUtils pwm;
-        //We have a servo in 0
-        while(1)
-        {
-            pwm.setServoPosition(0,testUltrasonic.getDistance(0)/40.0);
-
-
-        }
-    }
-
-    else if (strcmp(argv[1],"pwmservo")==0){
-        pwmUtils pwm;
-        //We have a servo in 0
-        while(1)
-        {
-            usleep(200000.0);
-            pwm.setServoPosition(0,0.3);
-            usleep(200000.0);
-            pwm.setServoPosition(0,0.5);
-        }
-    }    
-    else if (strcmp(argv[1],"sensormoduleultrasonic")==0){
-        sensorsModule mysensors;
-        while(1)
-        {
-            printf("%f\n", mysensors.frontUltrasonicData);
-            usleep(200000.0);
-
-        }
-    }
-
-    else if (strcmp(argv[1],"ultrasonicpidmotor")==0){
-        //test out pid control to make motors move to a fixed distance from wall, feedback with ultrasonic
-        ultrasonic testUltrasonic(2,3);
-        pwmUtils pwm;
-        pid testPID(1,0,0);
-        mraa::Gpio dirPin1(8);
-        mraa::Gpio dirPin2(9);
-
-        while(1){
-            int targetDist = 10; //get 10 cm away
-            float update = testPID.calcPID(targetDist,testUltrasonic.getDistance(1));
-            pwm.writePWM(1,std::abs(update)); //first motor PWM pin 1
-            if (update < 0){
-                dirPin1.write(0);
-                dirPin2.write(1);
-            }
-            else {
-                dirPin1.write(1);
-                dirPin2.write(0);
+    else{
+        if(strcmp(argv[1],"ultrasonic")==0){
+            printf("Running ultrasonic test\n");
+            ultrasonic testUltrasonic(2,3);
+            while(1)
+            {
+                usleep(200000.0);
+                printf("%f\n", testUltrasonic.getDistance(0));
             }
 
-            //first motor dir pin 8
-            printf("%f\n", update);
-            pwm.writePWM(2,std::abs(update)); //second motor PWM pin 2
-            //second motor dir pin 9
-
         }
-    }
-    else if (strcmp(argv[1],"testtime")==0){
-        sensorsModule mysensors;
-        while(1)
-        {
-            printf("%lld\n", mysensors.timeMicrosecondsSinceEpoch);
-            usleep(200000.0);
+        else if (strcmp(argv[1],"ultrasonicpwm")==0){
+            ultrasonic testUltrasonic(2,3);
+            pwmUtils pwm;
+            //We have a servo in 0
+            while(1)
+            {
+                pwm.setServoPosition(0,testUltrasonic.getDistance(0)/40.0);
 
+
+            }
         }
-    }
-    else if (strcmp(argv[1],"straightline")==0){
-        signal(SIGINT, stopMotors);
-        actuator myactuator;
-        actPointer= &myactuator;
-        sensorsModule mysensors;
-        motorsControl control(&mysensors);
-        control.desiredNormalizedAngularSpeed=0;
-        control.desiredNormalizedSpeed=0;
-        running=1;
-        while(running)
-        {
-            myactuator.setPowerLeftWheel(control.leftMotorPower);
-            myactuator.setPowerRightWheel(control.rightMotorPower);
-            printf("time =%lld\n", mysensors.timeMicrosecondsSinceEpoch);
-            printf("leftPower =%f\n", control.leftMotorPower);
-            printf("rightPower =%f\n", control.rightMotorPower);
-            printf("speed =%f\n", control.normalizedSpeed);
-            printf("angularspeed =%f\n", control.normalizedAngularSpeed);
-            printf("angle =%f\n", mysensors.gyroscopeAngle);
-            printf("rightRotation =%f\n", mysensors.rightEncoderRotations );
-            printf("leftRotation =%f\n", mysensors.leftEncoderRotations );
 
-            usleep(200000.0);
-
+        else if (strcmp(argv[1],"pwmservo")==0){
+            pwmUtils pwm;
+            //We have a servo in 0
+            while(1)
+            {
+                usleep(200000.0);
+                pwm.setServoPosition(0,0.3);
+                usleep(200000.0);
+                pwm.setServoPosition(0,0.5);
+            }
         }
-    }
-    else if (strcmp(argv[1],"motorcrazy")==0){
-        signal(SIGINT, stopMotors);
-        actuator myactuator;
-        actPointer = &myactuator;
-        running=1;
-        while(running)
-        {
-            myactuator.setPowerLeftWheel(1);
-            myactuator.setPowerRightWheel(1);
+        else if (strcmp(argv[1],"sensormoduleultrasonic")==0){
+            sensorsModule mysensors;
+            while(1)
+            {
+                printf("%f\n", mysensors.frontUltrasonicData);
+                usleep(200000.0);
 
-
-            usleep(200000.0);
-
+            }
         }
-    }
-    else if (strcmp(argv[1],"motorstop")==0){
-        signal(SIGINT, stopMotors);
-        actuator myactuator;
-        actPointer = &myactuator;
-        running=1;
-        while(running)
-        {
-            myactuator.setPowerLeftWheel(0);
-            myactuator.setPowerRightWheel(0);
 
+        else if (strcmp(argv[1],"ultrasonicpidmotor")==0){
+            //test out pid control to make motors move to a fixed distance from wall, feedback with ultrasonic
+            ultrasonic testUltrasonic(2,3);
+            pwmUtils pwm;
+            pid testPID(1,0,0);
+            mraa::Gpio dirPin1(8);
+            mraa::Gpio dirPin2(9);
 
-            usleep(200000.0);
+            while(1){
+                int targetDist = 10; //get 10 cm away
+                float update = testPID.calcPID(targetDist,testUltrasonic.getDistance(1));
+                pwm.writePWM(1,std::abs(update)); //first motor PWM pin 1
+                if (update < 0){
+                    dirPin1.write(0);
+                    dirPin2.write(1);
+                }
+                else {
+                    dirPin1.write(1);
+                    dirPin2.write(0);
+                }
 
+                //first motor dir pin 8
+                printf("%f\n", update);
+                pwm.writePWM(2,std::abs(update)); //second motor PWM pin 2
+                //second motor dir pin 9
+
+            }
         }
+        else if (strcmp(argv[1],"testtime")==0){
+            sensorsModule mysensors;
+            while(1)
+            {
+                printf("%lld\n", mysensors.timeMicrosecondsSinceEpoch);
+                usleep(200000.0);
+
+            }
+        }
+        else if (strcmp(argv[1],"straightline")==0){
+            signal(SIGINT, stopMotors);
+            actuator myactuator;
+            actPointer= &myactuator;
+            sensorsModule mysensors;
+            motorsControl control(&mysensors);
+            control.desiredNormalizedAngularSpeed=0;
+            control.desiredNormalizedSpeed=0;
+            running=1;
+            while(running)
+            {
+                myactuator.setPowerLeftWheel(control.leftMotorPower);
+                myactuator.setPowerRightWheel(control.rightMotorPower);
+                printf("time =%lld\n", mysensors.timeMicrosecondsSinceEpoch);
+                printf("leftPower =%f\n", control.leftMotorPower);
+                printf("rightPower =%f\n", control.rightMotorPower);
+                printf("speed =%f\n", control.normalizedSpeed);
+                printf("angularspeed =%f\n", control.normalizedAngularSpeed);
+                printf("angle =%f\n", mysensors.gyroscopeAngle);
+                printf("rightRotation =%f\n", mysensors.rightEncoderRotations );
+                printf("leftRotation =%f\n", mysensors.leftEncoderRotations );
+
+                usleep(200000.0);
+
+            }
+        }
+        else if (strcmp(argv[1],"motorcrazy")==0){
+            signal(SIGINT, stopMotors);
+            actuator myactuator;
+            actPointer = &myactuator;
+            running=1;
+            while(running)
+            {
+                myactuator.setPowerLeftWheel(1);
+                myactuator.setPowerRightWheel(1);
+
+
+                usleep(200000.0);
+
+            }
+        }
+        else if (strcmp(argv[1],"motorstop")==0){
+            signal(SIGINT, stopMotors);
+            actuator myactuator;
+            actPointer = &myactuator;
+            running=1;
+            while(running)
+            {
+                myactuator.setPowerLeftWheel(0);
+                myactuator.setPowerRightWheel(0);
+
+
+                usleep(200000.0);
+
+            }
+        }
+
+        else if (strcmp(argv[1],"encoder")==0){
+            encoder myenc(RIGHT_ENCODER_DIR,RIGHT_ENCODER_ENC);
+            while (1){
+                usleep(200000.0);
+                printf("right encoder counts =%lld\n", myenc.edgeCount);
+
+
+            }
+        }
+
+        return 0;
     }
-
-  else if (strcmp(argv[1],"encoder")==0){
-    encoder myenc(RIGHT_ENCODER_DIR,RIGHT_ENCODER_ENC);
-    while (1){
-        usleep(200000.0);
-        printf("right encoder counts =%lld\n", myenc.edgeCount);
-
-
-    }
-  }
-
-  return 0;
 }
 
 
