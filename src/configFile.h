33b4a13c3547dd9085b53b8403e3be79a2ff5468
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2015-01-17 06:53:45 -0500

    new angle for the state machine in the motor controller

--- src/configFile.h
+++ src/configFile.h
@@ -140,7 +140,7 @@
 #define ANG_SPEED_TOLERANCE 2  //angles per second
 #define POSITION_SPEED_TOLERANCE 0.001
 #define MINIMUM_THRESHOLD_PWM (0.03 / MAXIMUM_NORMALIZED_SAFE_SPEED_MOTORS) //Minimum pwm to move the motor at 1 safe factor
-#define MAXIMUM_DYNAMIC_TURN_ANGLE 30 //The maximum angle in which the robot can turn and move forward or back at the same time
+#define MAXIMUM_DYNAMIC_TURN_ANGLE 15 //The maximum angle in which the robot can turn and move forward or back at the same time
 
 //Maximum acceleration. Decrease if it is slipping
 #define CURRENT_LIMIT (0.1 / MAXIMUM_NORMALIZED_SAFE_SPEED_MOTORS)
