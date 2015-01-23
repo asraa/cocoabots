bf3812d3f0446f1ad5b00ff07ef54e99736e7282
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2015-01-23 17:53:38 -0500

    change hooke angle

--- src/actuators/servoscontrol.cpp
+++ src/actuators/servoscontrol.cpp
@@ -20,7 +20,7 @@ void servosControl::run(servosControl *myservo){
 }
 
 void servosControl::hookBlock(){
-    hookAngle = HOOK_START + 65;
+    hookAngle = HOOK_START + 90;
 }
 
 void servosControl::unHookBlock(){
