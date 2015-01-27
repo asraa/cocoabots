117efdb258d32e68e9db3fb5ebe2c0cc9d01667d
efjepsen <efjepsen@gmail.com>
2015-01-27 00:55:37 -0500

    rrt is the best thing ever now

--- src/mapping/objects/barrier.h
+++ src/mapping/objects/barrier.h
@@ -8,6 +8,10 @@ class barrier: public objectsSuperClass
 	public:
 		barrier(int ri, int rf);
 		void addStringVector(mapStringVector strVec);
+		int maxBarrierX = 0;
+		int maxBarrierY = 0; 
+		int minBarrierX = 222222;
+		int minBarrierY = 222222;
 };
 
 #endif
