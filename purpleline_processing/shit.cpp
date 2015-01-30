0ffbf987f2336620f761578a9dabf8acfcb5fcc5
efjepsen <efjepsen@gmail.com>
2015-01-30 02:03:24 -0500

    this should not be the main file

--- purpleline_processing/shit.cpp
+++ purpleline_processing/shit.cpp
@@ -57,9 +57,9 @@ void processFileLocalMap(std::string filename, int size) {
     src.copyTo(frame);
     TerritoryDetection::detectPurpleLine(frame,local_map);
     local_map_mat = local_map.cvtImage();
-    cv::HoughLines(local_map_mat,lines,5,5*CV_PI/180,100,0,0);
+    cv::HoughLines(local_map_mat,lines,4,4*CV_PI/180,25,0,0);
     cv::cvtColor(local_map_mat,local_map_mat,CV_GRAY2BGR);
-    addHoughLines(lines,local_map_mat,cv::Scalar(0,0,255),1,1000);
+    addHoughLines(lines,local_map_mat,cv::Scalar(0,0,255),1,50);
     std::cout << filename << ": number of lines = " << lines.size() << std::endl;
     displayMat(local_map_mat,"local_map");
     displayMat(src,"source");
@@ -79,11 +79,13 @@ void processFile(std::string filename) {
     std::vector<cv::Vec2f> contour_lines, canny_lines;
 
     src = cv::imread(filename,1);
+    src.copyTo(src_canny);
 
 
     // Generate empty matrix featuring canny lines of extra width
     cv::Canny(src,src_canny,50,200,3);
-//    displayMat(src_canny,"src_canny");
+//    ImageUtils::cannyEdge(src_canny);
+    displayMat(src_canny,"src_canny");
     src_canny.copyTo(contour_lines_mat);
     contour_lines_mat.setTo(cv::Scalar(0,0,0));
     cv::HoughLines(src_canny,canny_lines,1,CV_PI/180,150,0,0);
@@ -95,11 +97,10 @@ void processFile(std::string filename) {
     cv::cvtColor(purp_contour,purp_contour,CV_GRAY2BGR);
 
     std::cout << "size of canny_lines " << canny_lines.size() << std::endl;
-    // Binary AND operation between purp_contour and contour_lines_mat
-    //cv::Mat purp_contour_binand;
-    //purp_contour.copyTo(purp_contour_binand,contour_lines_mat);
-    //displayMat(purp_contour,"original purp_cotour");
-    //displayMat(purp_contour_binand,"after binand operation");
+//     Binary AND operation between purp_contour and contour_lines_mat
+    cv::Mat purp_contour_binand;
+    purp_contour.copyTo(purp_contour_binand,contour_lines_mat);
+    displayMat(purp_contour_binand,"after binand operation");
     cv::waitKey(5000);
 }
 
@@ -129,9 +130,9 @@ void processFile2(std::string filename) {
 }
 
 void processFile3(std::string filename) {
-    int threshold = 100;
-    int width = 10;
-    int factor = 100;
+    int threshold = 150;
+    int width = 5;
+    int factor = 10;
 
     cv::Mat src, src_canny, canny_lines_mat, tempMat;
     std::vector<cv::Vec2f> canny_lines;
@@ -145,8 +146,9 @@ void processFile3(std::string filename) {
 
     cv::Mat purp_contour = ColorDetection::detectColor(src, ColorDetection::COLOR_LINE_PURPLE);
 
+
     cv::Canny(src,src_canny,50,200,3);
-    cv::HoughLines(src_canny,canny_lines,1,CV_PI/180,threshold,0,0);
+    cv::HoughLines(purp_contour,canny_lines,1,CV_PI/180,threshold,0,0);
     //cv::Mat purp_contour = getPurpContour(src);
     cv::Mat all_canny_lines_mat;
     addHoughLines(canny_lines,src,cv::Scalar(0,0,255),width,factor);
@@ -155,57 +157,59 @@ void processFile3(std::string filename) {
 
     displayMat(src, "linez");
 
+    displayMat(purp_contour,"purp");
+
 //    for (int i = 0; i < canny_lines.size(); i++) {
 //        std::cout << "line " << i << "--" << std::endl;
 //        std::cout << "pt1: (" << canny_lines[i][0] << "," << canny_lines[i][1] << std::endl;
 //    }
 
 
-    if (canny_lines.size() == 0) {
-        std::cout << "fuck there aint any contours, sorry bud" << std::endl;
-    }
-    else {
-        cv::Mat maxMatContour, maxMatLine;
-        double maxVal = 0;
-        int maxLine = 0;
-        for (int i = 0; i < canny_lines.size(); i++) {
-            addHoughLine(canny_lines[i],canny_lines_mat,cv::Scalar(255,255,255),width,factor);
-            std::cout << "purp_countor size: " << purp_contour.rows << " by " << purp_contour.cols << std::endl;
-            std::cout << "canny_lines_mat size: " << canny_lines_mat.rows << " by " << canny_lines_mat.cols << std::endl;
-            purp_contour.convertTo(purp_contour,CV_8U);
-            canny_lines_mat.convertTo(canny_lines_mat,CV_8U);
-            tempMat.convertTo(tempMat,CV_8U);
-//            purp_contour.copyTo(tempMat,canny_lines_mat);
-            cv::bitwise_and(purp_contour,canny_lines_mat,tempMat);
-            double tempVal = cv::sum(tempMat)[0];
-            if (tempVal > maxVal) {
-                tempMat.copyTo(maxMatContour);
-                canny_lines_mat.copyTo(maxMatLine);
-                maxVal = tempVal;
-                maxLine = i;
-            }
-            tempMat.setTo(cv::Scalar(0,0,0));
-            canny_lines_mat.setTo(cv::Scalar(0,0,0));
-        }
-        std::cout << "The max line has area " << maxVal << std::endl;
-        displayMat(purp_contour,"purp_contours");
-        displayMat(all_canny_lines_mat, "all canny lines");
-        displayMat(maxMatContour,"max area contour");
-        displayMat(maxMatLine,"max area canny line");
-    }
+//    if (canny_lines.size() == 0) {
+//        std::cout << "fuck there aint any contours, sorry bud" << std::endl;
+//    }
+//    else {
+//        cv::Mat maxMatContour, maxMatLine;
+//        double maxVal = 0;
+//        int maxLine = 0;
+//        for (int i = 0; i < canny_lines.size(); i++) {
+//            addHoughLine(canny_lines[i],canny_lines_mat,cv::Scalar(255,255,255),width,factor);
+//            std::cout << "purp_countor size: " << purp_contour.rows << " by " << purp_contour.cols << std::endl;
+//            std::cout << "canny_lines_mat size: " << canny_lines_mat.rows << " by " << canny_lines_mat.cols << std::endl;
+//            purp_contour.convertTo(purp_contour,CV_8U);
+//            canny_lines_mat.convertTo(canny_lines_mat,CV_8U);
+//            tempMat.convertTo(tempMat,CV_8U);
+////            purp_contour.copyTo(tempMat,canny_lines_mat);
+//            cv::bitwise_and(purp_contour,canny_lines_mat,tempMat);
+//            double tempVal = cv::sum(tempMat)[0];
+//            if (tempVal > maxVal) {
+//                tempMat.copyTo(maxMatContour);
+//                canny_lines_mat.copyTo(maxMatLine);
+//                maxVal = tempVal;
+//                maxLine = i;
+//            }
+//            tempMat.setTo(cv::Scalar(0,0,0));
+//            canny_lines_mat.setTo(cv::Scalar(0,0,0));
+//        }
+//        std::cout << "The max line has area " << maxVal << std::endl;
+//        displayMat(purp_contour,"purp_contours");
+//        displayMat(all_canny_lines_mat, "all canny lines");
+//        displayMat(maxMatContour,"max area contour");
+//        displayMat(maxMatLine,"max area canny line");
+//    }
 
     cv::waitKey(100000);
 }
 
 
 
-int main(int argc, char** argv) {
-    int i = 47;
+int main_nope(int argc, char** argv) {
+    int i = 1;
     while (i <= 84) {
         i += 1;
         std::ostringstream oss;
         oss << "plines/plines_" << i << ".png";
-        processFile3(oss.str());
+        processFile(oss.str());
     }
     return 0;
 }
